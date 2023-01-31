439429ecc699fe853dc1aedd6666f75fecc350ee
congtran8498 <tranchicong08041998@gmail.com>
2023-01-31 16:10:26 +0700

    create BaiTap4

---
+++ Javacore_01/BaiTap4.java
@@ -0,0 +1,7 @@
+public class BaiTap4 {
+    public static void main(String[] args) {
+        int a = 15;
+        float ketQua =(float) Math.round(Math.sqrt(a)*1000)/1000 ;
+        System.out.println("khai căn số " +a+ " và làm tròn tới 3 chữ số thập phân là " +ketQua);
+    }
+}
