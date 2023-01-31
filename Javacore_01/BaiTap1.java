b98bea57e66194ed84306ac82c5aaf249b4de1c8
congtran8498 <tranchicong08041998@gmail.com>
2023-01-31 15:40:01 +0700

    Create BaiTap1.java

---
+++ Javacore_01/BaiTap1.java
@@ -0,0 +1,10 @@
+public class BaiTap1 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        int chuVi = (a+b)*2;
+        int dienTich = a*b;
+        System.out.println("chu vi của hình chữ nhật có chiều dài " +b+ " chiều rộng " +a+ " là " +chuVi);
+        System.out.println("diện tích của hình chữ nhật có chiều dài " +b+ " chiều rộng " +a+ " là " +dienTich);
+    }
+}
