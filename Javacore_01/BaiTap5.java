95fcb1bde0e52294705f8304f75c67a9cfd9436f
congtran8498 <tranchicong08041998@gmail.com>
2023-01-31 16:26:50 +0700

    Create BaiTap5.java

---
+++ Javacore_01/BaiTap5.java
@@ -0,0 +1,8 @@
+public class BaiTap5 {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 12;
+        float ketQua =(float) Math.round((float) a/b*1000)/1000;
+        System.out.println("Thương của " +a+","+b+ " và làm tròn 3 chữ số thập phân là " +ketQua);
+    }
+}
