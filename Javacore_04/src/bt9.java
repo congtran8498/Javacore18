e263ea7974b27260c8b1a9dd5c4a74eba91dba38
congtran8498 <tranchicong08041998@gmail.com>
2023-02-12 22:41:07 +0700

    new

--- Javacore_04/src/bt9.java
+++ Javacore_04/src/bt9.java
@@ -1,25 +1,16 @@
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class bt9 {
     public static void main(String[] args) {
-        int n = nhapSo("nhap so phan tu cua mang:");
-        int[] a = nhapMang(n);
+        int n = nhapSo("nhap so phan tu cua mang: ");
+        int[] a = themNgauNhien(n);
         xuatMang(a);
-    }
+        int[] b = inPhanTuKoTrungNhau(a);
+        xuatMang(b);
 
-    public static int nhapSo(String thongDiep) {
-        System.out.print(thongDiep);
-        return new Scanner(System.in).nextInt();
     }
-
-    public static int[] nhapMang(int n) {
-        int[] arr = new int[n];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = nhapSo("a[" + i + "]: ");
-        }
-        return arr;
-    }
-
     public static void xuatMang(int[] arr) {
         System.out.print("[");
         for (int i = 0; i < arr.length; i++) {
@@ -31,4 +22,37 @@ public class bt9 {
         System.out.println("]");
     }
 
+    public static int[] themNgauNhien(int n) {
+        int[] a = new int[n];
+        Random rd = new Random();
+        int count = 0;
+        while (count < n) {
+            a[count++] = rd.nextInt(100);
+        }
+        return a;
+    }
+
+    public static int[] inPhanTuKoTrungNhau(int[] arr) {
+        int [] brr = new int[arr.length];
+        int bSize = 0;
+        boolean isExist = false;
+        for (int i = 0; i < arr.length; i++) {
+            isExist = false;
+            for (int j = 0; j < bSize; j++) {
+                if(arr[i] == brr[j]){
+                    isExist = true;
+                    break;
+                }
+            }
+            if (!isExist){
+                brr[bSize++] = arr[i];
+            }
+        }
+        return brr;
+    }
+
+    public static int nhapSo(String thongDiep) {
+        System.out.print(thongDiep);
+        return new Scanner(System.in).nextInt();
+    }
 }
